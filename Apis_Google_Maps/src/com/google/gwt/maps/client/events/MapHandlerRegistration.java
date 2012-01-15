29c6bc8409339b741616b4ae7520ccfdc87a493d
branflake2267 <branflake2267@bd9e462a-c32e-cc58-439e-1be208b5d85e>
2012-01-15 23:13:51 +0000

    I added DomListener, in which can listen to controls events, but I'm not satisfied with it yet.

--- Apis_Google_Maps/src/com/google/gwt/maps/client/events/MapHandlerRegistration.java
+++ Apis_Google_Maps/src/com/google/gwt/maps/client/events/MapHandlerRegistration.java
@@ -5,12 +5,27 @@ import com.google.gwt.ajaxloader.client.Properties;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
+import com.google.gwt.dev.ui.UiEvent.Type;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.events.mapchange.MapChangeEventFormatter;
 
 public class MapHandlerRegistration {
 
-  public static <E extends MapEvent> HandlerRegistration addHandler(JavaScriptObject jso, MapEventType eventType, MapHandler<E> handler, MapEventFormatter<E> formatter) {
+  /**
+   * event handler 
+   * @param jso
+   * @param eventType
+   * @param handler
+   * @param formatter
+   * @return
+   */
+  public static <E extends MapEvent> HandlerRegistration addHandler(
+      JavaScriptObject jso, 
+      MapEventType eventType,
+      MapHandler<E> handler, 
+      MapEventFormatter<E> formatter) {
     final JavaScriptObject listener = addHandlerImpl(jso, eventType.value(), handler, formatter);
     HandlerRegistration registration = new HandlerRegistration() {
       public void removeHandler() { 
@@ -20,7 +35,19 @@ public class MapHandlerRegistration {
     return registration;
   }
   
-  public static <E extends MapEvent> HandlerRegistration addHandlerMvc(JavaScriptObject jso, MapEventType eventType, MapHandler<E> handler, MapEventFormatter<E> formatter) {
+  /**
+   * event handler for the mvc object
+   * @param jso
+   * @param eventType
+   * @param handler
+   * @param formatter
+   * @return
+   */
+  public static <E extends MapEvent> HandlerRegistration addHandlerMvc(
+      JavaScriptObject jso, 
+      MapEventType eventType, 
+      MapHandler<E> handler,
+      MapEventFormatter<E> formatter) {
     final JavaScriptObject listener = addHandlerImplMvc(jso, eventType.value(), handler, formatter);
     HandlerRegistration registration = new HandlerRegistration() {
       public void removeHandler() { 
@@ -30,7 +57,19 @@ public class MapHandlerRegistration {
     return registration;
   }
   
-  public static <E extends MapEvent> HandlerRegistration addHandlerDrawing(JavaScriptObject jso, MapEventType eventType, MapHandler<E> handler, MapEventFormatter<E> formatter) {
+  /**
+   * event handler for the drawing overlay objects
+   * @param jso
+   * @param eventType
+   * @param handler
+   * @param formatter
+   * @return
+   */
+  public static <E extends MapEvent> HandlerRegistration addHandlerDrawing(
+      JavaScriptObject jso, 
+      MapEventType eventType, 
+      MapHandler<E> handler, 
+      MapEventFormatter<E> formatter) {
     final JavaScriptObject listener = addHandlerImplDrawing(jso, eventType.value(), handler, formatter);
     HandlerRegistration registration = new HandlerRegistration() {
       public void removeHandler() { 
@@ -40,16 +79,38 @@ public class MapHandlerRegistration {
     return registration;
   } 
   
-  
-
-  private static native <E extends MapEvent> JavaScriptObject addHandlerImpl(JavaScriptObject jso, String eventName, MapHandler<E> handler, MapEventFormatter<E> formatter) /*-{
+  /**
+   * process the callback and send it to the handler
+   * @param jso
+   * @param eventName
+   * @param handler
+   * @param formatter
+   * @return
+   */
+  private static native <E extends MapEvent> JavaScriptObject addHandlerImpl(
+      JavaScriptObject jso, 
+      String eventName, 
+      MapHandler<E> handler, 
+      MapEventFormatter<E> formatter) /*-{
     var callback = function(event) { 
       @com.google.gwt.maps.client.events.MapHandlerRegistration::onCallback(Lcom/google/gwt/maps/client/events/MapHandler;Lcom/google/gwt/ajaxloader/client/Properties;Lcom/google/gwt/maps/client/events/MapEventFormatter;)(handler, event, formatter);
     };          
     return $wnd.google.maps.event.addListener(jso, eventName, callback)
   }-*/;
   
-  private static native <E extends MapEvent> JavaScriptObject addHandlerImplMvc(JavaScriptObject jso, String eventName, MapHandler<E> handler, MapEventFormatter<E> formatter) /*-{
+  /**
+   * process the callback and send it to the handler
+   * @param jso
+   * @param eventName
+   * @param handler
+   * @param formatter
+   * @return
+   */
+  private static native <E extends MapEvent> JavaScriptObject addHandlerImplMvc(
+      JavaScriptObject jso, 
+      String eventName, 
+      MapHandler<E> handler, 
+      MapEventFormatter<E> formatter) /*-{
     var callback = function(event) { 
       var eventCon = {
         index: event
@@ -67,7 +128,11 @@ public class MapHandlerRegistration {
    * @param formatter
    * @return
    */
-  private static native <E extends MapEvent> JavaScriptObject addHandlerImplDrawing(JavaScriptObject jso, String eventName, MapHandler<E> handler, MapEventFormatter<E> formatter) /*-{
+  private static native <E extends MapEvent> JavaScriptObject addHandlerImplDrawing(
+      JavaScriptObject jso, 
+      String eventName, 
+      MapHandler<E> handler, 
+      MapEventFormatter<E> formatter) /*-{
     var callback = function(event) { 
       var eventCon = {
         overlay: event
@@ -92,7 +157,10 @@ public class MapHandlerRegistration {
    * @param formatter
    */
   @SuppressWarnings("unused")
-  protected static <E extends MapEvent> void onCallback(final MapHandler<E> handler, final Properties properties, final MapEventFormatter<E> formatter) {
+  protected static <E extends MapEvent> void onCallback(
+      final MapHandler<E> handler, 
+      final Properties properties, 
+      final MapEventFormatter<E> formatter) {
     try {
       formatEvent(handler, properties, formatter);
     } catch (Throwable x) {
@@ -106,11 +174,18 @@ public class MapHandlerRegistration {
    * @param properties
    * @param formatter
    */
-  private static <E extends MapEvent> void formatEvent(MapHandler<E> handler, Properties properties,  MapEventFormatter<E> formatter) {
+  private static <E extends MapEvent> void formatEvent(
+      MapHandler<E> handler, 
+      Properties properties, 
+      MapEventFormatter<E> formatter) {
     E event = formatter.createEvent(properties);
     handler.onEvent(event);
   }
 
+  
+  
+  
+  
   /**
    * Triggers the given event. All arguments after eventName are passed as arguments to the listeners.
    * 
@@ -129,5 +204,42 @@ public class MapHandlerRegistration {
     var jso = w.@com.google.gwt.maps.client.MapWidget::getJso()();
     $wnd.google.maps.event.trigger(jso, eventName, args);
   }-*/;
+  
+  
+  
+  
+  
+  
+  /**
+   * experimental for the moment. I'm not satisfied as of yet with this strategy. 
+   * 
+   * Cross browser event handler registration. This listener is removed by calling removeListener(handle) for the handle that is returned by this function.
+   * @param jso
+   * @param type
+   * @param handler
+   * @param capture
+   */
+  public static <E> void addDomListener(
+      JavaScriptObject jso, 
+      com.google.gwt.event.dom.client.DomEvent.Type<ClickHandler> type, 
+      MapHandler<E> handler, 
+      boolean capture) {
+    addDomListener(jso, type.getName(), handler, capture);
+  }
 
+  private static native <E> void addDomListener(JavaScriptObject object, String eventName, MapHandler<?> handler, boolean capture) /*-{
+    var callback = function(event) {
+      @com.google.gwt.maps.client.events.MapHandlerRegistration::addDomListenerImpl(Lcom/google/gwt/ajaxloader/client/Properties;Lcom/google/gwt/maps/client/events/MapHandler;)(event, handler)
+    };
+    $wnd.google.maps.event.addDomListener(object, eventName, callback, capture);
+  }-*/;
+  
+  private static void addDomListenerImpl(Properties properties, MapHandler<MapEvent> handler) {
+    MapChangeEventFormatter formatter = new MapChangeEventFormatter();
+    handler.onEvent(formatter.createEvent(properties));
+  }
+  
+  
+  
+  
 }
