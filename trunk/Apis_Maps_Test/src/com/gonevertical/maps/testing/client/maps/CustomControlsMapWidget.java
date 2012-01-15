57c787ca8339756d772665daadd3214460655d65
branflake2267 <branflake2267@bd9e462a-c32e-cc58-439e-1be208b5d85e>
2012-01-15 23:11:53 +0000

    added click event handler to control overlay - example

--- trunk/Apis_Maps_Test/src/com/gonevertical/maps/testing/client/maps/CustomControlsMapWidget.java
+++ trunk/Apis_Maps_Test/src/com/gonevertical/maps/testing/client/maps/CustomControlsMapWidget.java
@@ -1,8 +1,12 @@
 package com.gonevertical.maps.testing.client.maps;
 
+import com.google.gwt.cell.client.ClickableTextCell;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.maps.client.MapImpl;
 import com.google.gwt.maps.client.MapOptions;
 import com.google.gwt.maps.client.MapTypeId;
@@ -12,6 +16,9 @@ import com.google.gwt.maps.client.adsense.AdUnitOptions;
 import com.google.gwt.maps.client.adsense.AdUnitWidget;
 import com.google.gwt.maps.client.base.LatLng;
 import com.google.gwt.maps.client.controls.ControlPosition;
+import com.google.gwt.maps.client.events.MapEvent;
+import com.google.gwt.maps.client.events.MapHandler;
+import com.google.gwt.maps.client.events.MapHandlerRegistration;
 import com.google.gwt.maps.client.events.MouseEvent;
 import com.google.gwt.maps.client.events.channelnumber.ChannelNumberChangeMapEvent;
 import com.google.gwt.maps.client.events.channelnumber.ChannelNumberChangeMapHandler;
@@ -31,6 +38,7 @@ import com.google.gwt.maps.client.overlays.InfoWindowOptions;
 import com.google.gwt.maps.client.overlays.Marker;
 import com.google.gwt.maps.client.overlays.MarkerOptions;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
@@ -77,28 +85,44 @@ public class CustomControlsMapWidget extends Composite {
     
     mapWidget.addClickHandler(new ClickMapHandler() {
       public void onEvent(ClickMapEvent event) {
-        // TODO fix the event getting, getting ....
-        System.out.println("clicked on latlng=" + event.getMouseEvent().getLatLng());
       }
     });
   }
   
+  
   private void drawControls() {
     
     Button button = new Button("test");
-    button.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-       System.out.println("clicked");
-      }
-    });
+    CheckBox cb = new CheckBox();
+  
     
     FlowPanel widget = new FlowPanel();
     widget.add(button);
     widget.add(new HTML("test"));
     widget.add(button);
+    widget.add(cb);
     
     mapWidget.setControls(ControlPosition.RIGHT_CENTER, widget);
+    
+    
+    JavaScriptObject jso = button.getElement();
+    MapHandler<MapEvent> handler = new MapHandler<MapEvent>() {
+      public void onEvent(MapEvent event) {
+        System.out.println("clicked on button");
+      }
+    };
+    MapHandlerRegistration.addDomListener(jso, ClickEvent.getType(), handler, true);
+
+    
+    MapHandler<MapEvent> handler2 = new MapHandler<MapEvent>() {
+      public void onEvent(MapEvent event) {
+        System.out.println("clicked on checkbox");
+      }
+    };
+    MapHandlerRegistration.addDomListener(cb.getElement(), ClickEvent.getType(), handler2, true);
+    
   }
 
+  
   
 }
